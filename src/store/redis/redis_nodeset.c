23ae4fa37673f7c22ee65654f95cf1c980e95a85
rong fengliang <1141591465@qq.com>
2023-03-17 22:33:38 +0800

    fix

--- src/store/redis/redis_nodeset.c
+++ src/store/redis/redis_nodeset.c
@@ -2587,7 +2587,10 @@ static void node_connector_callback(redisAsyncContext *ac, void *rep, void *priv
         return node_connector_fail(node, "is busy loading data...");
       }
       if(nchan_cstr_match_line(reply->str, "role:active-replica")) {
-          return;
+          node_set_role(node, REDIS_NODE_ROLE_MASTER);
+          if(!node->cluster.enabled && !node_discover_slaves_from_info_reply(node, reply)) {
+              return node_connector_fail(node, "failed parsing slaves from INFO");
+          }
       }
       if(nchan_cstr_match_line(reply->str, "cluster_enabled:1")) {
         node->cluster.enabled = 1;
